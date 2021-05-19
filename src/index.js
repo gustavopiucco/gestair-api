57cad6231380656d7df27c72fee58397cef5e8d5
Gustavo Piucco <gustavopiucco@gmail.com>
2021-05-19 14:35:07 -0300

    API base

---
+++ src/index.js
@@ -0,0 +1,40 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+const app = require('./app');
+
+let server;
+
+(async () => {
+    try {
+        server = app.listen(process.env.port, () => console.info(`Listening to port ${process.env.port}`));
+    }
+    catch (error) {
+        console.error(error);
+    }
+})();
+
+const exitHandler = () => {
+    if (server) {
+        server.close(() => {
+            logger.info('Server closed');
+            process.exit(1);
+        });
+    } else {
+        process.exit(1);
+    }
+};
+
+const unexpectedErrorHandler = (error) => {
+    logger.error(error);
+    exitHandler();
+};
+
+process.on('uncaughtException', unexpectedErrorHandler);
+process.on('unhandledRejection', unexpectedErrorHandler);
+
+process.on('SIGTERM', () => {
+    logger.info('SIGTERM received');
+    if (server) {
+        server.close();
+    }
+});
