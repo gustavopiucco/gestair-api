5b5dc8a9fe29fae1a7e2eb29e182500259380a2d
Gustavo Piucco <gustavopiucco@gmail.com>
2021-05-26 01:18:36 -0300

    fix

--- src/database/mysql.js
+++ src/database/mysql.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     connectionLimit: 10,
     host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USERR,
+    user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
 });
