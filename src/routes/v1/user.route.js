0379f966f6ba0be5846e47284fa14d7501b5a2a0
Gustavo Piucco <gustavopiucco@gmail.com>
2021-06-05 20:35:19 -0300

    Public register

--- src/routes/v1/user.route.js
+++ src/routes/v1/user.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const auth = require('../../middlewares/auth');
 const userController = require('../../controllers/user.controller');
 
-router.post('/', auth('create_user'), userController.createUser);
+router.post('/', userController.createUser);
 router.get('/', auth('get_user'), userController.getUser);
 
 module.exports = router;
