e89e4fcc42599174fe4b41e1a00f7da33d9a72bb
Gustavo Piucco <gustavopiucco@gmail.com>
2021-06-09 10:12:38 -0300

    fix

--- src/config/roles.js
+++ src/config/roles.js
@@ -2,7 +2,7 @@ const roles = ['admin', 'company_manager'];
 
 const rolePermissions = new Map();
 rolePermissions.set(roles[0], ['create_user', 'update_user_company', 'update_user_customer', 'admin_create_company']);
-rolePermissions.set(roles[1], ['create_customer']);
+rolePermissions.set(roles[1], ['create_customer', 'update_user_customer']);
 
 module.exports = {
     roles,
