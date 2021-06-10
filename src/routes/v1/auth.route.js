03068f447182bee4382af4a1da3e9d3d1124e0b0
Gustavo Piucco <gustavopiucco@gmail.com>
2021-06-10 04:33:38 -0300

    Input validation with JOI

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const validate = require('../../middlewares/validate');
+const authValidation = require('../../validations/auth.validation');
 const authController = require('../../controllers/auth.controller');
 
-router.post('/login', authController.login);
+router.post('/login', validate(authValidation.login), authController.login);
 
 module.exports = router;
