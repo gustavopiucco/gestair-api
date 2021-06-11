034268c0db7dd993b37f83ecb3fd70ffcfcf27cd
Gustavo Piucco <gustavopiucco@gmail.com>
2021-06-11 09:22:49 -0300

    clean roles

--- src/config/roles.js
+++ src/config/roles.js
@@ -1,9 +1,26 @@
 const roles = ['admin', 'company_manager', 'company_unit_manager'];
 
 const rolePermissions = new Map();
-rolePermissions.set(roles[0], ['create_user', 'get_user', 'update_user_company', 'update_user_customer', 'admin_create_company', 'create_unit']);
-rolePermissions.set(roles[1], ['create_customer', 'update_user_customer']);
-rolePermissions.set(roles[2], ['create_unit']);
+
+rolePermissions.set(roles[0],
+    [
+        'create_user',
+        'get_user',
+        'update_user_company',
+        'update_user_customer',
+        'admin_create_company',
+        'create_unit',
+        'update_user'
+    ]);
+rolePermissions.set(roles[1],
+    [
+        'create_customer',
+        'update_user_customer'
+    ]);
+rolePermissions.set(roles[2],
+    [
+        'create_unit'
+    ]);
 
 module.exports = {
     roles,
