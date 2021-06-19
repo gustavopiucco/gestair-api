1a00163fe5fc4d2f5d7912f6903d8f6502c014b1
Gustavo Piucco <gustavopiucco@gmail.com>
2021-06-18 21:09:36 -0300

    clean

--- src/database/data.sql
+++ src/database/data.sql
@@ -20,6 +20,26 @@ CREATE TABLE customers (
   CONSTRAINT fk_customers_company_id_companies_id FOREIGN KEY (company_id) REFERENCES companies (id)
 ) ENGINE=InnoDB;
 
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  email varchar(100) NOT NULL,
+  password_hash char(60) NOT NULL,
+  type enum('admin', 'company', 'customer'),
+  role enum('admin', 'company_manager'),
+  first_name varchar(20) NOT NULL,
+  last_name varchar(60) NOT NULL,
+  cpf char(11) NOT NULL,
+  phone varchar(20) NOT NULL,
+  company_id int,
+  customer_id int,
+  CONSTRAINT pk_id PRIMARY KEY (id),
+  CONSTRAINT uc_email UNIQUE (email),
+  CONSTRAINT uc_cpf UNIQUE (cpf),
+  CONSTRAINT chk_company_id_customer_id CHECK ((company_id IS NOT NULL AND customer_id IS NULL) OR (customer_id IS NOT NULL AND company_id IS NULL) OR (company_id IS NULL AND customer_id IS NULL)),
+  CONSTRAINT fk_users_company_id_companies_id FOREIGN KEY (company_id) REFERENCES companies (id),
+  CONSTRAINT fk_users_customer_id_customers_id FOREIGN KEY (customer_id) REFERENCES customers (id)
+) ENGINE=InnoDB;
+
 CREATE TABLE units (
   id int NOT NULL AUTO_INCREMENT,
   name varchar(100) NOT NULL,
@@ -62,22 +82,11 @@ CREATE TABLE physical_equipments (
   CONSTRAINT fk_physical_equipments_enviroments_id_enviroments_id FOREIGN KEY (enviroment_id) REFERENCES enviroments (id)
 ) ENGINE=InnoDB;
 
-CREATE TABLE users (
-  id int NOT NULL AUTO_INCREMENT,
-  email varchar(100) NOT NULL,
-  password_hash char(60) NOT NULL,
-  type enum('admin', 'company', 'customer'),
-  role enum('admin', 'company_manager'),
-  first_name varchar(20) NOT NULL,
-  last_name varchar(60) NOT NULL,
-  cpf char(11) NOT NULL,
-  phone varchar(20) NOT NULL,
-  company_id int,
-  customer_id int,
-  PRIMARY KEY (id),
-  UNIQUE KEY (email),
-  UNIQUE KEY (cpf),
-  CONSTRAINT chk_company_id_customer_id CHECK ((company_id IS NOT NULL AND customer_id IS NULL) OR (customer_id IS NOT NULL AND company_id IS NULL) OR (company_id IS NULL AND customer_id IS NULL)),
-  CONSTRAINT fk_users_company_id_companies_id FOREIGN KEY (company_id) REFERENCES companies (id),
-  CONSTRAINT fk_users_customer_id_customers_id FOREIGN KEY (customer_id) REFERENCES customers (id)
+CREATE TABLE users_work_time (
+  user_id int NOT NULL,
+  week_day enum('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday') NOT NULL,
+  work_from time NOT NULL,
+  work_to time NOT NULL,
+  CONSTRAINT uc_users_work_time UNIQUE (user_id, week_day, work_from, work_to),
+  CONSTRAINT fk_users_working_time_user_id_users_id FOREIGN KEY (user_id) REFERENCES users (id)
 ) ENGINE=InnoDB;
