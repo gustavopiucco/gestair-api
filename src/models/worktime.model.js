e58cfa573114d758efc8448232ce8037ab1df75d
Gustavo Piucco <gustavopiucco@gmail.com>
2021-06-22 14:25:18 -0300

    Work time fix

--- src/models/worktime.model.js
+++ src/models/worktime.model.js
@@ -1,5 +1,15 @@
 const mysql = require('../database/mysql');
 
+async function workTimeExists(userId, weekDay, workFrom, workTo) {
+    const result = await mysql.execute('SELECT 1 FROM work_time WHERE user_id = ? AND week_day = ? AND work_from = ? and work_to = ?', [userId, weekDay, workFrom, workTo]);
+    return result.length > 0;
+}
+
+async function workTimeIntervalExists(userId, weekDay, workFrom, workTo) {
+    const result = await mysql.execute('SELECT 1 FROM work_time WHERE user_id = ? AND week_day = ? AND ? BETWEEN work_from AND work_to OR ? BETWEEN work_from AND work_to', [userId, weekDay, workFrom, workTo]);
+    return result.length > 0;
+}
+
 async function getWorkTimeById(id) {
     const result = await mysql.execute('SELECT * FROM work_time WHERE id = ?', [id]);
     return result[0];
@@ -10,6 +20,8 @@ async function createUserWorkTime(id, weekDay, workFrom, workTo) {
 }
 
 module.exports = {
+    workTimeExists,
+    workTimeIntervalExists,
     getWorkTimeById,
     createUserWorkTime,
 }
--- src/services/worktime.service.js
+++ src/services/worktime.service.js
@@ -4,6 +4,18 @@ const userModel = require('../models/user.model');
 const workTimeModel = require('../models/worktime.model');
 
 async function createUserWorkTime(body) {
+    if (body.workFrom > '23:59' || body.workTo > '23:59') {
+        throw new ApiError(httpStatus.BAD_REQUEST, 'Os horários devem ser informados entre 00:00 e 23:59');
+    }
+
+    if (body.workTo <= body.workFrom) {
+        throw new ApiError(httpStatus.BAD_REQUEST, 'O horário final não pode ser menor ou igual ao horário inicial');
+    }
+
+    if (await workTimeModel.workTimeExists(body.userId, body.weekDay, body.workFrom + ':00', body.workTo + ':00')) {
+        throw new ApiError(httpStatus.BAD_REQUEST, 'Está carga horária já está cadastrada para este usuário');
+    }
+
     const user = await userModel.getUserById(body.userId);
 
     if (!user) {
