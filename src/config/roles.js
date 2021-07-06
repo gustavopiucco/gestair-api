17324ad70fd4623f78fcda2e8f62197a92a7d090
Gustavo Piucco <gustavopiucco@gmail.com>
2021-07-06 18:57:33 -0300

    company_admin role + TODO

--- src/config/roles.js
+++ src/config/roles.js
@@ -1,4 +1,4 @@
-const roles = ['admin', 'company_manager', 'company_technician', 'customer_manager'];
+const roles = ['admin', 'company_admin', 'company_manager', 'company_technician', 'customer_manager'];
 
 const rolePermissions = new Map();
 
@@ -32,7 +32,39 @@ rolePermissions.set(roles[0],
         'set_maintenance_plan',
         'create_unit_technician_link'
     ]);
+
 rolePermissions.set(roles[1],
+    [
+        'create_user',
+        'get_user',
+        'admin_create_company',
+        'create_unit',
+        'update_user',
+        'set_company_technician',
+        'set_customer_manager',
+        'create_work_time',
+        'get_work_time',
+        'delete_work_time',
+        'get_all_users_by_company',
+        'create_maintenance_plan',
+        'get_maintenance_plans',
+        'create_activity',
+        'get_activities',
+        'create_checklist',
+        'get_checklists',
+        'delete_activity',
+        'delete_checklist',
+        'get_all_customers',
+        'get_all_units',
+        'create_enviroment',
+        'get_all_enviroments',
+        'create_equipment',
+        'get_all_equipments',
+        'set_maintenance_plan',
+        'create_unit_technician_link'
+    ]);
+
+rolePermissions.set(roles[2],
     [
         'get_user',
         'create_customer',
@@ -61,11 +93,13 @@ rolePermissions.set(roles[1],
         'set_maintenance_plan',
         'create_unit_technician_link'
     ]);
-rolePermissions.set(roles[2],
+
+rolePermissions.set(roles[3],
     [
 
     ]);
-rolePermissions.set(roles[3],
+
+rolePermissions.set(roles[4],
     [
 
     ]);
