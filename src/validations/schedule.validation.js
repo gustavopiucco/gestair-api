524e6580fcf5f503abe45e0c467158f14c2cd103
Gustavo Piucco <gustavopiucco@gmail.com>
2021-07-13 19:18:13 -0300

    clean

--- src/validations/schedule.validation.js
+++ src/validations/schedule.validation.js
@@ -3,6 +3,7 @@ const { date } = require('./custom.validation');
 
 const create = {
     body: Joi.object().keys({
+        maintenancePlanId: Joi.number().integer().required(),
         startDate: Joi.string().required().custom(date),
     }),
 };
